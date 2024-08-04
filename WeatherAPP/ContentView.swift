6c2700b1aa8c53fdad1105b57eacaf1570dfdadb
Michał Talaga <michal.talaga.programming@gmail.com>
2024-08-04 17:20:06 +0200

    UI

--- WeatherAPP/ContentView.swift
+++ WeatherAPP/ContentView.swift
@@ -1,12 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var cityName = ""
+    @State private var cityName = "Zembrzyce"
     @State private var weatherData: WeatherData?
-
     var body: some View {
         NavigationView {
-            VStack(spacing: 0) {
+            VStack {
                             HStack {
                                 GeometryReader { geometry in
                                     HStack(spacing: 3) {
@@ -32,10 +31,11 @@ struct ContentView: View {
                                     }
                                 }
                             }
-
+                            .border(Color.gray, width: 2)
+                
                 if let weatherData = weatherData {
-                    ScrollView {
-                        VStack(alignment: .leading, spacing: 20) {
+                        VStack( spacing: 20) {
+                            
                             // City Information Block
                             VStack(alignment: .leading, spacing: 5) {
                                 Text("\(weatherData.city.name)")
@@ -45,60 +45,47 @@ struct ContentView: View {
                                 Text("Wschód słońca: \(formatDate(timestamp: weatherData.city.sunrise))")
                                 Text("Zachód słońca: \(formatDate(timestamp: weatherData.city.sunset))")
                             }
-                            .padding()
-                            .background(Color.blue.opacity(0.1))
-                            .cornerRadius(8)
+                                .padding()
+                                .background(Color.blue.opacity(0.1))
+                                .cornerRadius(8)
 
                             // Current Weather Block
                             if let currentWeather = weatherData.list.first {
                                 VStack(alignment: .leading, spacing: 5) {
-                                    Text("Pogoda teraz")
-                                        .font(.headline)
                                     Text("Temperatura: \(currentWeather.main.temp) K")
                                     Text("Odczuwalna temperatura: \(currentWeather.main.feels_like) K")
-                                    Text("Temperatura min: \(currentWeather.main.temp_min) K")
-                                    Text("Temperatura max: \(currentWeather.main.temp_max) K")
-                                    Text("Ciśnienie: \(currentWeather.main.pressure) hPa")
-                                    Text("Wilgotność: \(currentWeather.main.humidity)%")
-                                    Text("Widoczność: \(currentWeather.visibility) m")
                                     Text("Zachmurzenie: \(currentWeather.clouds.all)%")
-                                    Text("Wiatr: \(currentWeather.wind.speed) m/s, kierunek: \(currentWeather.wind.deg)°")
-                                    if let gust = currentWeather.wind.gust {
-                                        Text("Podmuchy wiatru: \(gust) m/s")
-                                    }
                                     Text("Opis pogody: \(currentWeather.weather.first?.description ?? "")")
                                 }
-                                .padding()
-                                .background(Color.green.opacity(0.1))
-                                .cornerRadius(8)
+                                    .padding()
+                                    .background(Color.green.opacity(0.1))
+                                    .cornerRadius(8)
                             }
 
                             // Weather Forecast Block
-                            VStack(alignment: .leading, spacing: 5) {
-                                Text("Pogoda na następne 9 godzin")
-                                    .font(.headline)
-                                ForEach(weatherData.list.prefix(3), id: \.dt) { item in
-                                    VStack(alignment: .leading, spacing: 5) {
-                                        Text("Data: \(item.dt_txt)")
-                                        Text("Temperatura: \(item.main.temp) K")
-                                        Text("Odczuwalna temperatura: \(item.main.feels_like) K")
-                                        Text("Ciśnienie: \(item.main.pressure) hPa")
-                                        Text("Wilgotność: \(item.main.humidity)%")
-                                        Text("Zachmurzenie: \(item.clouds.all)%")
-                                        Text("Wiatr: \(item.wind.speed) m/s, kierunek: \(item.wind.deg)°")
-                                        if let gust = item.wind.gust {
-                                            Text("Podmuchy wiatru: \(gust) m/s")
+                            ScrollView(.horizontal) {
+                                HStack(alignment: .top, spacing: 10) {
+                                    ForEach(weatherData.list.prefix(10), id: \.dt) { item in
+                                        VStack(alignment: .leading, spacing: 0) {
+                                            Text(item.dt_txt)
+                                                .font(.subheadline)
+                                            Text("\(item.main.temp) K")
+                                            Text("\(item.main.feels_like) K")
+                                            Text("\(item.clouds.all)%")
+                                            Text("\(item.weather.first?.description ?? "")")
                                         }
-                                        Text("Opis pogody: \(item.weather.first?.description ?? "")")
+                                        .padding()
+                                        .background(Color.orange.opacity(0.1))
+                                        .cornerRadius(8)
+                                        .frame(width: 150) // Ustal szerokość karty
                                     }
-                                    .padding()
-                                    .background(Color.orange.opacity(0.1))
-                                    .cornerRadius(8)
                                 }
                             }
+                            
+                            
                         }
                         .padding()
-                    }
+                    
                 }
             }
             .padding()
