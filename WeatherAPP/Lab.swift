eac52b3c72a03e3fd6bd28ff2fa4f7495442b8fe
Michał Talaga <michal.talaga.programming@gmail.com>
2024-08-06 14:19:52 +0200

    creating Lab file where im working on improvements of UI

---
+++ WeatherAPP/Lab.swift
@@ -0,0 +1,18 @@
+//
+//  Lab.swift
+//  WeatherAPP
+//
+//  Created by Michał Talaga on 06/08/2024.
+//
+
+import SwiftUI
+
+struct Lab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Lab()
+}
