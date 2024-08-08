1daa50787db4965fd0dadc6d575c68501bb5f5fa
Michał Talaga <michal.talaga.programming@gmail.com>
2024-08-08 14:00:43 +0200

    start with widgets

---
+++ WeatherWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  WeatherWidget
+//
+//  Created by Michał Talaga on 08/08/2024.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ WeatherWidget/WeatherWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWidgetBundle.swift
+//  WeatherWidget
+//
+//  Created by Michał Talaga on 08/08/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WeatherWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WeatherWidget()
+        WeatherWidgetLiveActivity()
+    }
+}
