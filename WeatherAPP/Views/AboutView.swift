ded47b5e2371347af2dd302214aa3d689dc9198d
Michał Talaga <michal.talaga.programming@gmail.com>
2024-08-16 14:51:46 +0200

    update

---
+++ WeatherAPP/Views/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  WeatherAPP
+//
+//  Created by Michał Talaga on 16/08/2024.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
---
+++ WeatherAPP/Views/PrivacyPolicy.swift
@@ -0,0 +1,18 @@
+//
+//  PrivacyPolicy.swift
+//  WeatherAPP
+//
+//  Created by Michał Talaga on 16/08/2024.
+//
+
+import SwiftUI
+
+struct PrivacyPolicy: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrivacyPolicy()
+}
