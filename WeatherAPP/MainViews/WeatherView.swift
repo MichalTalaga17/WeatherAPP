c2979f80c9e8a35fddc5e1578ff7eddc2249bf66
Michał Talaga <michal.talaga.programming@gmail.com>
2024-08-17 13:43:46 +0200

    update

--- WeatherAPP/MainViews/WeatherView.swift
+++ WeatherAPP/MainViews/WeatherView.swift
@@ -16,105 +16,42 @@ struct WeatherView: View {
     @State private var pollution: PollutionData?
     @State private var errorMessage: String?
     
+    var cityName: String? 
+    
     var body: some View {
         VStack {
-            if let location = locationManager.location {
+            if let cityName = cityName {
+                Text("City: \(cityName)")
+                    .font(.title)
+                    .padding(.bottom)
+                
+                if let weather = currentWeather {
+                    WeatherDetailsView(weather: weather)
+                }
+                
+                if let forecast = forecast {
+                    ForecastView(forecast: forecast)
+                }
+                
+                if let pollution = pollution {
+                    AirPollutionView(pollution: pollution)
+                }
+                
+            } else if let location = locationManager.location {
                 Text("City: \(locationManager.cityName)")
                     .font(.title)
                     .padding(.bottom)
                 
-                Text("Latitude: \(location.coordinate.latitude)")
-                Text("Longitude: \(location.coordinate.longitude)")
-                
                 if let weather = currentWeather {
-                    VStack(alignment: .leading) {
-                        Text("Current Weather")
-                            .font(.headline)
-                            .padding(.top)
-                        
-                        Text("Temperature: \(Int(weather.main.temp))°C")
-                        Text("Feels Like: \(Int(weather.main.feels_like))°C")
-                        Text("Min Temp: \(Int(weather.main.temp_min))°C")
-                        Text("Max Temp: \(Int(weather.main.temp_max))°C")
-                        Text("Humidity: \(weather.main.humidity)%")
-                        Text("Pressure: \(weather.main.pressure) hPa")
-                        Text("Cloudiness: \(weather.clouds.all)%")
-                        Text("Visibility: \(weather.visibility / 1000) km")
-                        Text("Wind Speed: \(Int(weather.wind.speed)) m/s")
-                        
-                        if let rain = weather.rain?.hour1 {
-                            Text("Rain: \(rain) mm (1h)")
-                        }
-                        if let snow = weather.snow?.hour1 {
-                            Text("Snow: \(snow) mm (1h)")
-                        }
-                        
-                        Text("Sunrise: \(formatUnixTimeToHourAndMinute(weather.sys.sunrise, timezone: weather.timezone))")
-                        Text("Sunset: \(formatUnixTimeToHourAndMinute(weather.sys.sunset, timezone: weather.timezone))")
-                        
-                        if let weatherDescription = weather.weather.first?.description {
-                            Text("Description: \(weatherDescription.capitalized)")
-                        }
-                        if let icon = weather.weather.first?.icon {
-                            IconConvert(for: icon, useWeatherColors: iconsColorsBasedOnWeather)
-                        }
-                    }
+                    WeatherDetailsView(weather: weather)
                 }
                 
                 if let forecast = forecast {
-                    VStack(alignment: .leading) {
-                        Text("Forecast")
-                            .font(.headline)
-                            .padding(.top)
-                        
-                        ScrollView(.horizontal, showsIndicators: false){
-                            HStack{
-                                ForEach(forecast.list.prefix(10), id: \.dt) { entry in
-                                    VStack{
-                                        Text("\(extractHour(from: entry.dt_txt))")
-                                        IconConvert(for: entry.weather.first?.icon ?? "", useWeatherColors: iconsColorsBasedOnWeather)
-                                        Text("\(Int(entry.main.temp))°")
-                                        Text("\(Int(entry.main.feels_like))°")
-                                        
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    .padding()
+                    ForecastView(forecast: forecast)
                 }
                 
                 if let pollution = pollution {
-                        
-                    ScrollView(.horizontal){
-                            if let pollutionEntry = pollution.list.first {
-                                    HStack{
-                                        VStack{
-                                            Text("\(Int(pollutionEntry.components.pm2_5))")
-                                            Text("PM2.5")
-                                        }
-                                        VStack{
-                                            Text("\(Int(pollutionEntry.components.pm10))")
-                                            Text("PM10")
-                                        }
-                                        VStack {
-                                            Text("\(Int(pollutionEntry.main.aqi))")
-                                            Text("AQI")
-                                        }
-                                        VStack{
-                                            Text("\(Int(pollutionEntry.components.co))")
-                                            Text("CO")
-                                        }
-                                        VStack{
-                                            Text("\(Int(pollutionEntry.components.no2))")
-                                            Text("NO2")
-                                        }
-                                        
-                                    }
-                            }
-                        
-                    }
-                    .padding()
+                    AirPollutionView(pollution: pollution)
                 }
                 
             } else {
@@ -130,8 +67,10 @@ struct WeatherView: View {
         .padding()
         .onAppear {
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                if locationManager.cityName != "Unknown" {
-                    fetchWeatherData()
+                if let cityName = cityName {
+                    fetchWeatherData(for: cityName)
+                } else if locationManager.cityName != "Unknown" {
+                    fetchWeatherData(for: locationManager.cityName)
                 } else {
                     errorMessage = "Unable to determine city name."
                 }
@@ -139,9 +78,7 @@ struct WeatherView: View {
         }
     }
     
-    private func fetchWeatherData() {
-        let city = locationManager.cityName
-        
+    private func fetchWeatherData(for city: String) {
         if city != "Unknown" {
             API.shared.fetchCurrentWeatherData(forCity: city) { result in
                 switch result {
@@ -175,6 +112,105 @@ struct WeatherView: View {
     }
 }
 
-#Preview {
-    WeatherView()
+struct WeatherDetailsView: View {
+    let weather: CurrentData
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Current Weather")
+                .font(.headline)
+                .padding(.top)
+            
+            Text("Temperature: \(Int(weather.main.temp))°C")
+            Text("Feels Like: \(Int(weather.main.feels_like))°C")
+            Text("Min Temp: \(Int(weather.main.temp_min))°C")
+            Text("Max Temp: \(Int(weather.main.temp_max))°C")
+            Text("Humidity: \(weather.main.humidity)%")
+            Text("Pressure: \(weather.main.pressure) hPa")
+            Text("Cloudiness: \(weather.clouds.all)%")
+            Text("Visibility: \(weather.visibility / 1000) km")
+            Text("Wind Speed: \(Int(weather.wind.speed)) m/s")
+            
+            if let rain = weather.rain?.hour1 {
+                Text("Rain: \(rain) mm (1h)")
+            }
+            if let snow = weather.snow?.hour1 {
+                Text("Snow: \(snow) mm (1h)")
+            }
+            
+            Text("Sunrise: \(formatUnixTimeToHourAndMinute(weather.sys.sunrise, timezone: weather.timezone))")
+            Text("Sunset: \(formatUnixTimeToHourAndMinute(weather.sys.sunset, timezone: weather.timezone))")
+            
+            if let weatherDescription = weather.weather.first?.description {
+                Text("Description: \(weatherDescription.capitalized)")
+            }
+            if let icon = weather.weather.first?.icon {
+                IconConvert(for: icon, useWeatherColors: true)
+            }
+        }
+    }
+}
+
+struct ForecastView: View {
+    let forecast: ForecastData
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Forecast")
+                .font(.headline)
+                .padding(.top)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(forecast.list.prefix(10), id: \.dt) { entry in
+                        VStack {
+                            Text("\(extractHour(from: entry.dt_txt))")
+                            IconConvert(for: entry.weather.first?.icon ?? "", useWeatherColors: true)
+                            Text("\(Int(entry.main.temp))°")
+                            Text("\(Int(entry.main.feels_like))°")
+                        }
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct AirPollutionView: View {
+    let pollution: PollutionData
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            if let pollutionEntry = pollution.list.first {
+                HStack {
+                    VStack {
+                        Text("\(Int(pollutionEntry.components.pm2_5))")
+                        Text("PM2.5")
+                    }
+                    VStack {
+                        Text("\(Int(pollutionEntry.components.pm10))")
+                        Text("PM10")
+                    }
+                    VStack {
+                        Text("\(Int(pollutionEntry.main.aqi))")
+                        Text("AQI")
+                    }
+                    VStack {
+                        Text("\(Int(pollutionEntry.components.co))")
+                        Text("CO")
+                    }
+                    VStack {
+                        Text("\(Int(pollutionEntry.components.no2))")
+                        Text("NO2")
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview{
+    WeatherView(cityName: "Zembrzyce")
 }
